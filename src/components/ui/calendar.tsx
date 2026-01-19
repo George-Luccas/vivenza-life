1147a64efe74eac7496b5cbadc27791bbfab96c2
George-Luccas <georgeluccas300@gmail.com>
2026-01-18 20:18:55 -0400

    fix: explicit flex layout restoration for calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -27,19 +27,19 @@ function Calendar({
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-8 w-8 bg-transparent p-0 text-muted-foreground hover:opacity-100 hover:text-primary hover:bg-rose-50 rounded-full transition-all"
+          "h-7 w-7 bg-transparent p-0 text-muted-foreground hover:opacity-100 hover:text-primary hover:bg-rose-50 rounded-full"
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "",
+        head_row: "flex",
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
-        row: "w-full mt-2",
+        row: "flex w-full mt-2",
         cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-rose-50/50 [&:has([aria-selected])]:bg-rose-50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-9 w-9 p-0 font-normal aria-selected:opacity-100 rounded-full hover:bg-rose-100 hover:text-rose-900 transition-colors"
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100 rounded-full hover:bg-rose-100 hover:text-rose-900"
         ),
         day_range_end: "day-range-end",
         day_selected:
